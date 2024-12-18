942ffa905be5a6f3c33fabd844b31045e6b34aa5
Jacob Finkelman <jfinkelm@amazon.com>
2024-12-18 17:24:40 +0000

    conflict_count

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,8 @@ use hasher::StableHasher;
 use itertools::Itertools as _;
 use names::{new_bucket, new_links, new_wide, FeatureNamespace, Names};
 use pubgrub::{
-    resolve, Dependencies, DependencyConstraints, DependencyProvider, PubGrubError,
-    SelectedDependencies, VersionSet,
+    resolve, Dependencies, DependencyConstraints, DependencyProvider, PackageResolutionStatistics,
+    PubGrubError, SelectedDependencies, VersionSet,
 };
 use rc_semver_pubgrub::RcSemverPubgrub;
 use ron::ser::PrettyConfig;
@@ -228,7 +228,7 @@ impl<'c> Index<'c> {
         range: &RcSemverPubgrub,
         package: &Q,
         req: &&semver::VersionReq,
-    ) -> usize
+    ) -> u32
     where
         Q: ?Sized + Hash + Eq,
         InternedString: std::borrow::Borrow<Q>,
@@ -243,11 +243,11 @@ impl<'c> Index<'c> {
                 .dedup()
                 .map(|v| v.canonical())
                 .filter(|v| range.contains(v))
-                .count()
+                .count() as u32
         }
     }
 
-    fn count_matches<Q>(&self, range: &RcSemverPubgrub, package: &Q) -> usize
+    fn count_matches<Q>(&self, range: &RcSemverPubgrub, package: &Q) -> u32
     where
         Q: ?Sized + Hash + Eq,
         InternedString: std::borrow::Borrow<Q>,
@@ -257,7 +257,7 @@ impl<'c> Index<'c> {
         } else {
             self.get_versions(package)
                 .filter(|v| range.contains(v))
-                .count()
+                .count() as u32
         }
     }
 
@@ -581,30 +581,39 @@ impl<'c> DependencyProvider for Index<'c> {
         })
     }
 
-    type Priority = Reverse<usize>;
+    type Priority = (u32, Reverse<u32>);
 
-    fn prioritize(&self, package: &Names<'c>, range: &RcSemverPubgrub) -> Self::Priority {
-        Reverse(match package {
-            Names::Links(_name) => {
-                // PubGrub automatically handles when any requirement has no overlap. So this is only deciding a importance of picking the version:
-                //
-                // - If it only matches one thing, then adding the decision with no additional dependencies makes no difference.
-                // - If it can match more than one thing, and it is entirely equivalent to picking the packages directly which would make more sense to the users.
-                //
-                // So only rubberstamp links attributes when all other decisions are made, by setting the priority as low as it will go.
-                usize::MAX
-            }
+    fn prioritize(
+        &self,
+        package: &Names<'c>,
+        range: &RcSemverPubgrub,
+        conflict_count: &PackageResolutionStatistics,
+    ) -> Self::Priority {
+        (
+            conflict_count.conflict_count(),
+            Reverse(match package {
+                Names::Links(_name) => {
+                    // PubGrub automatically handles when any requirement has no overlap. So this is only deciding a importance of picking the version:
+                    //
+                    // - If it only matches one thing, then adding the decision with no additional dependencies makes no difference.
+                    // - If it can match more than one thing, and it is entirely equivalent to picking the packages directly which would make more sense to the users.
+                    //
+                    // So only rubberstamp links attributes when all other decisions are made, by setting the priority as low as it will go.
+                    u32::MAX
+                }
 
-            Names::Wide(_, req, _, _) => self.count_wide_matches(range, &package.crate_(), req),
-            Names::WideFeatures(_, req, _, _, _) | Names::WideDefaultFeatures(_, req, _, _) => self
-                .count_wide_matches(range, &package.crate_(), req)
-                .saturating_add(1),
+                Names::Wide(_, req, _, _) => self.count_wide_matches(range, &package.crate_(), req),
+                Names::WideFeatures(_, req, _, _, _) | Names::WideDefaultFeatures(_, req, _, _) => {
+                    self.count_wide_matches(range, &package.crate_(), req)
+                        .saturating_add(1)
+                }
 
-            Names::Bucket(_, _, _) => self.count_matches(range, &package.crate_()),
-            Names::BucketFeatures(_, _, _) | Names::BucketDefaultFeatures(_, _) => self
-                .count_matches(range, &package.crate_())
-                .saturating_add(1),
-        })
+                Names::Bucket(_, _, _) => self.count_matches(range, &package.crate_()),
+                Names::BucketFeatures(_, _, _) | Names::BucketDefaultFeatures(_, _) => self
+                    .count_matches(range, &package.crate_())
+                    .saturating_add(1),
+            }),
+        )
     }
 
     fn get_dependencies(
